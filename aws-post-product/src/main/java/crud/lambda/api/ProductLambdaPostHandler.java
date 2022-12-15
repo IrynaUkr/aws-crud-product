497ef1fbbbca4e79d35bd7dc13b25f8a779a96bc
irinapodoprigora <ukrira@gmail.com>
2022-12-15 20:05:51 +0000

    added put handler

--- aws-post-product/src/main/java/crud/lambda/api/ProductLambdaPostHandler.java
+++ aws-post-product/src/main/java/crud/lambda/api/ProductLambdaPostHandler.java
@@ -5,6 +5,9 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import crud.lambda.api.model.Product;
@@ -17,6 +20,7 @@ import java.io.*;
 public class ProductLambdaPostHandler implements RequestStreamHandler {
 
     private static final String DYNAMO_TABLE = "Products";
+
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
@@ -50,15 +54,14 @@ public class ProductLambdaPostHandler implements RequestStreamHandler {
                     responseObject.put("statusCode", 400);
                 }
             }
-
         } catch (Exception e) {
             responseObject.put("statusCode", 400);
             responseObject.put("error", e);
         }
-
         writer.write(responseObject.toString());
         reader.close();
         writer.close();
     }
-
 }
+
+
--- aws-put-product/src/main/java/crud/lambda/api/ProductLambdaPutHandler.java
+++ aws-put-product/src/main/java/crud/lambda/api/ProductLambdaPutHandler.java
@@ -3,8 +3,10 @@ package crud.lambda.api;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
+import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
+import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import crud.lambda.api.model.Product;
@@ -12,6 +14,8 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ProductLambdaPutHandler implements RequestStreamHandler {
@@ -35,31 +39,33 @@ public class ProductLambdaPutHandler implements RequestStreamHandler {
             if (reqObject.get("body") != null) {
                 Product product = new Product((String) reqObject.get("body"));
 
-                Item item = dynamoDB.getTable(DYNAMO_TABLE).getItem("id", product.getId());
-                if (item == null) {
+                Map<String, String> expressionAttributeNames = new HashMap<String, String>();
+                expressionAttributeNames.put("#name", "name");
+                UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                        .withPrimaryKey("id", product.getId())
+                        .withUpdateExpression("set #na = :name")
+                        .withNameMap(new NameMap()
+                                .with("#na", "name"))
+                        .withValueMap(new ValueMap()
+                                .withString(":name", product.getName()))
+                        .withReturnValues(ReturnValue.ALL_NEW);
+                dynamoDB.getTable(DYNAMO_TABLE).updateItem(updateItemSpec);
 
-                    dynamoDB.getTable(DYNAMO_TABLE)
-                            .putItem(new PutItemSpec().withItem(new Item()
-                                    .withNumber("id", product.getId())
-                                    .withString("name", product.getName())
-                                    .withNumber("price", product.getPrice())));
-                    responseBody.put("message", "New Item created");
-                    responseObject.put("statusCode", 200);
-                    responseObject.put("body", responseBody.toString());
-                } else {
-                    responseObject.put("statusCode", 400);
-                }
+                responseBody.put("message", " Item updated");
+                responseObject.put("statusCode", 200);
+                responseObject.put("body", responseBody.toString());
+            } else {
+                responseObject.put("statusCode", 400);
+                responseBody.put("message", " body of request is null");
             }
-
-
         } catch (Exception e) {
-            responseObject.put("statusCode", 400);
+            responseObject.put("statusCode", 428);
+            responseBody.put("message", " exception occurs ");
             responseObject.put("error", e);
         }
-
         writer.write(responseObject.toString());
         reader.close();
         writer.close();
     }
-}
 
+}
